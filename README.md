018c5bf872e30ce48a8bfa682150f54453586065
Gabriel <alves.go.co@gmail.com>
2025-07-24 16:53:49 +0200

    Testing fixed

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# dutoolbox
+#### Testing
+
+Testing can be run with
+
+```
+chmod +x test.sh
+test.sh
+```
+
+Alternatively, one may also run
+
+```
+pytest test/test_du.py::TestClassName
+```
+
+to test individual classes.
--- tests/test_du.py
+++ tests/test_du.py
@@ -1,40 +1,42 @@
-import numpy as np
-from ..src import dual_unitary as du
-# import .dual_unitary as du
+# pytest dutoolbox/test/test_du.py
 
-class TestCircuit:
+import numpy as np
+from ..dualunitary import *
+from ..integrable import *
+
+class TestIntegrableCircuit:
     def test_permutator(self):
         
-        pi_m = du.permutator([1, 2, 3, 4, 5, 6, 7])
+        pi_m = permutator([1, 2, 3, 4, 5, 6, 7])
         pi_m_expected = np.array([2, 4, 1, 6, 3, 7, 5])
         
         assert (pi_m_expected == pi_m).all(), "Incorrect permutation operator"
         
-    def test_correspondent_statates(self):
+    def test_correspondent_states(self):
         
         state_B = [1, 1, 0, 0, 0]
         state_A = [1, 0, 1, 0, 0]
         
-        assert du.correspondent_statates(state_A, state_B)
+        assert correspondent_states(state_A, state_B)
     
     def test_soliton_action(self):
      
         a = np.array([1, 1, 1])
         m = np.array([1, 1, 0])
     
-        assert du.soliton_action(a, m) == -1
+        assert soliton_action(a, m) == -1
     
     def test_charge_action(self):
         
         a = np.array([1, 0, 1, 0, 0])
         m = np.array([1, 0, 0, 0, 1])
         
-        assert du.charge_action(a, m) == -3, "Incorrect quantum number"
+        assert charge_action(a, m) == -3, "Incorrect quantum number"
     
     def test_scattering_matrix(self):
     
         L = 7
-        V = du.scattering_matrix(L)
+        V = scattering_matrix(L)
         expect_V = np.array([[0, 1, 0, 1, 0, 0, 0], 
                              [0, 0, 0, 0, 0, 0, 0], 
                              [0, 0, 0, 1, 0, 1, 0], 
@@ -46,7 +48,7 @@ class TestCircuit:
         assert (V == expect_V).all()
     
     def test_cumulative_scattering(self):
-        phi_s = du.cumulative_scattering([1, 1, 0, 0, 0])
+        phi_s = cumulative_scattering([1, 1, 0, 0, 0])
         expected_phi_s = np.array([0, 1, 1, 2, 2, 2])
     
         assert (phi_s == expected_phi_s).all(), "Incorrect (cumulative) partial phase scattering list"
@@ -57,18 +59,18 @@ class TestCircuit:
         L = 5
         rep_states =  np.array([[0, 1, 0, 0, 0], [0, 1, 0, 1, 0], [0, 1, 0, 0, 1], [0, 1, 0, 1, 1], [0, 1, 1, 1, 0], [0, 1, 1, 1, 1]])
     
-        assert (np.array(du.all_representatives_FKM(L)) == rep_states).all()
+        assert (np.array(all_representatives_FKM(L)) == rep_states).all()
 
 class TestNecklaces:
     def test_get_representatives(self):
     
         # Checks if we get (L m)/L representatives
-        assert len(du.get_representatives(4, 11)) == 30, "Incorrect number of representative states for L = 11 and m = 4"
+        assert len(get_representatives(4, 11)) == 30, "Incorrect number of representative states for L = 11 and m = 4"
         
         # For L = 5 and m = 2. Check if states themselves are correct
         representatives = np.array([[0, 1, 0, 1, 0], [0, 1, 0, 0, 1]])
         
-        assert (du.get_representatives(2, 5) == representatives).all(), "Incorrect representative states. An equivalent representation might have been used."
+        assert (get_representatives(2, 5) == representatives).all(), "Incorrect representative states. An equivalent representation might have been used."
     
     
     def test_generate_necklaces(self):
@@ -80,11 +82,11 @@ class TestNecklaces:
                            [0, 1, 1, 1], 
                            [1, 1, 1, 1]]
         
-        assert list(du.generate_necklaces(2, 4)) == representatives
+        assert list(generate_necklaces(2, 4)) == representatives
         
     def test_sector_crossing(self):
     
         sector_representation = [1, 1, 0, 0, 0]
         
-        assert du.sector_crossing(sector_representation, delete_diagonal=True) == 0.3584
-        assert du.sector_crossing(sector_representation, delete_diagonal=False) == 0.36, "Incorrect crossing. Possibly wrong diagonal contributions?"
+        assert sector_crossing(sector_representation, delete_diagonal=True) == 0.3584
+        assert sector_crossing(sector_representation, delete_diagonal=False) == 0.36, "Incorrect crossing. Possibly wrong diagonal contributions?"
